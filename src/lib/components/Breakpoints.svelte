eb08626fd03c30869c002006cd63d7c7ac2786e8
ampled <korsneseirik@gmail.com>
2023-04-30 14:15:47 +0200

    tailwind breakpoints monitor component

---
+++ src/lib/components/Breakpoints.svelte
@@ -0,0 +1,13 @@
+<script>
+	let className = '';
+	export { className as class };
+</script>
+
+<div class={`fixed bottom-0 left-0 flex flex-col font-mono px-4 bg-green-300 ${className}`}>
+	<div class="bg-green-400">xs</div>
+	<div class="bg-red-400 sm:bg-green-400">sm</div>
+	<div class="bg-red-400 lg:bg-green-400">md</div>
+	<div class="bg-red-400 lg:bg-green-400">lg</div>
+	<div class="bg-red-400 xl:bg-green-400">xl</div>
+	<div class="bg-red-400 2xl:bg-green-400">2xl</div>
+</div>
