71413a6fb01eff754620c97ea6d9465a46d02612
ampled <korsneseirik@gmail.com>
2023-04-30 14:14:55 +0200

    code highlight component

---
+++ src/dev-lib/Code.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import Highlight, { LineNumbers, HighlightSvelte } from 'svelte-highlight';
+	import typescript from 'svelte-highlight/languages/typescript';
+	// import html from 'svelte-highlight/languages/ht';
+	import dracula from 'svelte-highlight/styles/monokai';
+
+	export let code: string = '';
+	export let svelte = true;
+</script>
+
+<svelte:head>
+	{@html dracula}
+</svelte:head>
+
+{#if svelte}
+	<HighlightSvelte {code} let:highlighted class="overflow-auto">
+		<LineNumbers {highlighted} wrapLines />
+	</HighlightSvelte>
+{:else}
+	<Highlight language={typescript} {code} let:highlighted class="overflow-auto">
+		<LineNumbers {highlighted} wrapLines />
+	</Highlight>
+{/if}
