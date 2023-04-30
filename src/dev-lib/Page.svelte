024f24ae5581b1ac01ff274e8c34e56ddc0bc620
ampled <korsneseirik@gmail.com>
2023-04-30 14:15:23 +0200

    reusable page layout

---
+++ src/dev-lib/Page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { ROUTE_TRANSITION, ROUTE_TRANSITION_PARAMS } from './const';
+
+	export let title = '';
+</script>
+
+<div
+	class="flex flex-col items-center justify-start h-full w-full gap-4"
+	in:ROUTE_TRANSITION={ROUTE_TRANSITION_PARAMS}
+>
+	<h1 class="font-bold text-3xl">{title}</h1>
+	<slot />
+</div>
