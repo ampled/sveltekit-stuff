a63c081f8312e1229f142eae868f14c74cf6a63c
ampled <korsneseirik@gmail.com>
2023-04-30 14:15:12 +0200

    reusable text input

---
+++ src/dev-lib/TextInput.svelte
@@ -0,0 +1,9 @@
+<script>
+	export let title = '';
+	export let value = '';
+</script>
+
+<label class="flex flex-col min-w-[320px] w-[600px]">
+	<b>{title}</b>
+	<input type="text" class="border border-black rounded" bind:value />
+</label>
