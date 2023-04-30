ebd2aa74818b518f3f506a89d8268c63f5737019
ampled <korsneseirik@gmail.com>
2023-04-30 14:16:07 +0200

    default export

--- src/lib/util/merge.ts
+++ src/lib/util/merge.ts
@@ -2,6 +2,6 @@ import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
 export { ClassValue };
-export function merge(...inputs: ClassValue[]): string {
+export default function merge(...inputs: ClassValue[]): string {
 	return twMerge(clsx(inputs));
 }
