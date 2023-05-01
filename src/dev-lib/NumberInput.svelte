66e7342c03f309b692ada2b927bf5dc9a591d8aa
ampled <korsneseirik@gmail.com>
2023-05-01 12:28:36 +0200

    number input

---
+++ src/dev-lib/NumberInput.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import type { Optional } from '$lib';
+	import type { HTMLInputAttributes } from 'svelte/elements';
+
+	type $$Props = {
+		title?: string;
+		value?: number;
+		slider?: boolean;
+	} & HTMLInputAttributes;
+
+	export let title: Optional<string> = '';
+	export let value: Optional<number> = undefined;
+	export let slider: Optional<boolean> = true;
+</script>
+
+<label class="flex flex-col">
+	<b>{title}</b>
+	{#if slider}
+		<input type="range" bind:value {...$$restProps} />
+	{/if}
+	<input class="border border-black rounded" type="number" bind:value {...$$restProps} />
+</label>
