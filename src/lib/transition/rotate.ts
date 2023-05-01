a4bbb92d14ffb9f31afe1ecc2ae9a6557664d691
ampled <korsneseirik@gmail.com>
2023-05-01 12:29:23 +0200

    add x/y params to rotate transition

--- src/lib/transition/rotate.ts
+++ src/lib/transition/rotate.ts
@@ -1,31 +1,44 @@
+import { splitCssUnit } from '../util';
 import { backOut } from 'svelte/easing';
 import type { EasingFunction, TransitionConfig } from 'svelte/transition';
 
 export interface RotateParams {
 	delay?: number;
 	duration?: number;
-	/** @default backOut */
 	easing?: EasingFunction;
-	/** @default 0 */
 	opacity?: number;
-	/**  @default 180  */
-	rotation?: number;
+	rotation?: number | string;
+	x?: number;
+	y?: number;
 }
 
 export function rotate(
 	node: Element,
-	{ delay = 0, duration = 250, easing = backOut, rotation = 180, opacity = 0 }: RotateParams = {}
+	{
+		delay = 0,
+		duration = 250,
+		easing = backOut,
+		rotation = 180,
+		opacity = 0,
+		x = 0,
+		y = 0
+	}: RotateParams = {}
 ): TransitionConfig {
 	const style = getComputedStyle(node);
 	const target_opacity = +style.opacity;
+	const transform = style.transform === 'none' ? '' : style.transform;
 	const od = target_opacity * (1 - opacity);
-
+	const [rotateValue, rotateUnit] = splitCssUnit(rotation, 'deg');
+	const [xValue, xUnit] = splitCssUnit(x);
+	const [yValue, yUnit] = splitCssUnit(y);
 	return {
 		delay,
 		duration,
 		easing,
 		css: (t, u) => `
-			transform: rotate(${(1 - t) * rotation}deg);
+			rotate: ${(1 - t) * rotateValue}${rotateUnit};
+			transform: ${transform}
+				translate(${(1 - t) * xValue}${xUnit}, ${(1 - t) * yValue}${yUnit});
 			opacity: ${target_opacity - od * u}`
 	};
 }
