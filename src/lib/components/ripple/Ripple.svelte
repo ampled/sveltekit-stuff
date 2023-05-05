4eca00a661117c3c153502a3c9d40acc8353cc04
ampled <korsneseirik@gmail.com>
2023-05-05 15:35:36 +0200

    rippledip

--- src/lib/components/ripple/Ripple.svelte
+++ src/lib/components/ripple/Ripple.svelte
@@ -1,16 +1,21 @@
 <script lang="ts">
-	import { backOut } from 'svelte/easing';
+	import { backOut, linear } from 'svelte/easing';
 	import { onMount } from 'svelte/types/runtime/internal/lifecycle';
-	// const defaults: RippleInstanceOptions = {
-	//   easing: backOut,
-	//   spreadDuration: 400,
-	//   opacity: 0.1,
-	//   center: false,
-	// }
+	let rippleInstances: RippleInstanceOptions[] = [];
+	let ele: HTMLDivElement;
 
-	type RippleInstanceParams = {};
+	const defaults: RippleInstanceOptions = {
+		easing: backOut,
+		spreadDuration: 400,
+		opacity: 0.1,
+		center: false,
+		clearDelay: 0,
+		clearEasing: linear,
+		color: 'currentColor'
+	};
 
 	type RippleInstanceOptions = {
+		id?: string;
 		easing: (t: number) => number;
 		spreadDuration: number;
 		center: boolean;
@@ -20,13 +25,37 @@
 		clearEasing: any;
 	};
 
-	function createRipple() {}
-	function addRipple() {}
+	export let options: Partial<RippleInstanceOptions> = defaults;
 
-	onMount(() => {});
+	$: rippleOptions = { ...defaults, ...options };
+
+	function createRipple(id: string) {
+		return {
+			id,
+			...rippleOptions
+		} satisfies RippleInstanceOptions;
+	}
+	function addRipple(e: Event) {
+		const id = Date.now().toString();
+		const ripple = createRipple(id);
+		rippleInstances = [...rippleInstances, ripple];
+		setTimeout(() => {
+			removeRipple(id);
+		}, options.spreadDuration);
+	}
+
+	function removeRipple(id: string) {
+		rippleInstances = rippleInstances.filter((ripple) => ripple.id != id);
+	}
+
+	onMount(() => {
+		ele.addEventListener('pointerdown', addRipple);
+
+		return;
+	});
 </script>
 
-<div class="esks-ripples" />
+<div bind:this={ele} class="esks-ripples" />
 
 <style>
 	:global(.esks-ripple-container) {
